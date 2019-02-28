f2c0514bc45d4da64b19c7e99b5432247abd2578
xia0m <alexma325@gmail.com>
2019-02-28 11:40:18 -0800

    finished dicard function and test in strain

--- strain/strain.ts
+++ strain/strain.ts
@@ -3,5 +3,12 @@ export function keep<T>(value: T[], callback: (n: T) => boolean): T[] {
 }
 
 export function discard<T>(value: T[], callback: (n: T) => boolean): T[] {
-  return value.filter(callback)
+  const temp: T[] = value.filter(callback)
+  const arr: T[] = []
+  for (let i = 0; i < value.length; i++) {
+    if (temp.indexOf(value[i]) === -1) {
+      arr.push(value[i])
+    }
+  }
+  return arr
 }
