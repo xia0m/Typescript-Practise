9418a21ec864faca1c0678a5d9a8a5b2c9c8692a
xia0m <alexma325@gmail.com>
2019-04-02 15:55:57 -0700

    modified leap year solution

--- leap/leap.ts
+++ leap/leap.ts
@@ -1,14 +1,5 @@
 function isLeapYear(year: number) {
-    // Your code here
-    if (year % 4 !== 0) {
-        return false
-    } else if (year % 100 !== 0) {
-        return true
-    } else if (year % 400 !== 0) {
-        return false
-    } else {
-        return true
-    }
+  return (year % 4 === 0 && year % 100 !== 0) || year % 400 === 0
 }
 
 export default isLeapYear
