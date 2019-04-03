ca37540fab995053a708a2d182965804e0fa1a13
xia0m <alexma325@gmail.com>
2019-04-03 16:52:24 -0700

    implement random key generator

--- simple-cipher/simple-cipher.ts
+++ simple-cipher/simple-cipher.ts
@@ -1,11 +1,36 @@
 class SimpleCipher {
-    encode( /* Parameters go here */ ) {
-        // Your code here
-    }
+  public key: string
+  public KEY_LENGTH: number = 100
+  public CHARACTER_SET = "abcdefghijklmnopqrstuvwxyz"
 
-    decode( /* Parameters go here */ ) {
-        // Your code here
+  constructor(key?: string) {
+    key
+      ? (this.key = this.validatedKey(key))
+      : (this.key = this.generateRandomKey())
+  }
+
+  validatedKey(key: string): string {
+    return key
+  }
+
+  generateRandomKey(): string {
+    let text: string = ""
+
+    for (let i = 0; i < this.KEY_LENGTH; i++) {
+      text += this.CHARACTER_SET.charAt(
+        Math.floor(Math.random() * this.CHARACTER_SET.length)
+      )
     }
+    return text
+  }
+
+  encode(message: string): string {
+    return message
+  }
+
+  decode(message: string): string {
+    return message
+  }
 }
 
 export default SimpleCipher
