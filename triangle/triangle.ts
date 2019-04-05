6492fb1ad288e72f5723c304b7538b3b0ce25263
xia0m <alexma325@gmail.com>
2019-04-04 23:45:33 -0700

    finished triangle

--- triangle/triangle.ts
+++ triangle/triangle.ts
@@ -1,12 +1,39 @@
 export default class Triangle {
+  sides: number[]
 
-    sides: number[]
+  constructor(...sides: number[]) {
+    this.sides = sides
+  }
 
-    constructor(...sides: number[]) {
-        this.sides = sides
+  checkInput(sides: number[]): boolean {
+    if (sides[0] === 0 || sides[1] === 0 || sides[2] === 0) {
+      return false
+    }
+    if (
+      sides[0] + sides[1] <= sides[2] ||
+      sides[1] + sides[2] <= sides[0] ||
+      sides[0] + sides[2] <= sides[1]
+    ) {
+      return false
     }
 
-    kind() {
-        return ""
+    return true
+  }
+
+  kind(): string {
+    if (!this.checkInput(this.sides)) {
+      throw new Error()
     }
+    if (this.sides[0] === this.sides[1] && this.sides[1] === this.sides[2]) {
+      return "equilateral"
+    } else if (
+      this.sides[0] === this.sides[1] ||
+      this.sides[1] === this.sides[2] ||
+      this.sides[0] === this.sides[2]
+    ) {
+      return "isosceles"
+    } else {
+      return "scalene"
+    }
+  }
 }
