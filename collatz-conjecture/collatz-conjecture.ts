e51ed2abeb1f05be3da49eb5d7dbca452cf6c6db
xia0m <alexma325@gmail.com>
2019-04-05 17:33:39 -0700

    finished collatz-conjecture

--- collatz-conjecture/collatz-conjecture.ts
+++ collatz-conjecture/collatz-conjecture.ts
@@ -1,7 +1,20 @@
 class CollatzConjecture {
-    static steps( /* Parameters go here */ ) {
-        // Your code here
+  static steps(num: number): number {
+    if (num <= 0) {
+      throw new Error("Only positive numbers are allowed")
     }
+    let numSteps: number = 0
+    while (num !== 1) {
+      // check whether num is even or odd
+      // if it is even, num = num /2
+      // if it is odd, num = num*3 + 1
+      num % 2 === 0 ? (num /= 2) : (num = num * 3 + 1)
+      numSteps++
+      // increse the actual steps by 1
+    }
+
+    return numSteps
+  }
 }
 
 export default CollatzConjecture
